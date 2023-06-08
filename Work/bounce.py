6eb769cb2adf1fcb1a65c9888457e99c34b9a527
kgb965 <kourtney.boston@austin.utexas.edu>
2023-06-08 10:15:58 -0500

    my own solutions so far to sears, bounce, and mortgage problems

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,14 @@
 # bounce.py
 #
 # Exercise 1.5
+start_height = 100.00
+end_height = 0.0
+num_bounces = 10
+i = 1 
+
+
+for i in range(num_bounces): 
+   end_height = start_height * .6
+   print(i, round(end_height, 4))
+   start_height = end_height
+   i = i + 1
