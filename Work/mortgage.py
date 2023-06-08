f2961ad3b3fd94184380a9651d7032ed2050c289
kgb965 <kourtney.boston@austin.utexas.edu>
2023-06-08 11:18:47 -0500

    Looked at solution to determine why I had 2 extra payments on my program and corrected. Solved extra credit 1.11

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -10,24 +10,38 @@ extra_payment_start_month = 61
 extra_payment_end_month = 108
 months_paid = 0
 
-for i in range(60):
-    principal = principal * (1+rate/12) - payment
-    total_paid = total_paid + payment
-    months_paid = months_paid + 1
-    print(months_paid, 'Total paid', round(total_paid, 2),'Principal', round(principal,2))
-#print('Total paid', round(total_paid, 2), 'Months paid', months_paid)
+# for i in range(60):
+#     principal = principal * (1+rate/12) - payment
+#     total_paid = total_paid + payment
+#     months_paid = months_paid + 1
+#     print(months_paid, 'Total paid', round(total_paid, 2),'Principal', round(principal,2))
+# #print('Total paid', round(total_paid, 2), 'Months paid', months_paid)
 
-for i in range(extra_payment_start_month,extra_payment_end_month):
-    principal = principal * (1+rate/12) - (payment + extra_payment)
-    total_paid = total_paid + payment + extra_payment
-    months_paid = months_paid + 1
-    print(months_paid, 'Total paid', round(total_paid, 2),'Principal', round(principal,2))
-#print('Total paid for first 12 months', round(total_paid, 2), months_paid)
+# for i in range(extra_payment_start_month,extra_payment_end_month):
+#     principal = principal * (1+rate/12) - (payment + extra_payment)
+#     total_paid = total_paid + payment + extra_payment
+#     months_paid = months_paid + 1
+#     print(months_paid, 'Total paid', round(total_paid, 2),'Principal', round(principal,2))
+# #print('Total paid for first 12 months', round(total_paid, 2), months_paid)
 
 while principal > 0:
+   
+
     principal = principal * (1+rate/12) - payment
     total_paid = total_paid + payment
     months_paid = months_paid + 1
+    
+    if months_paid >= extra_payment_start_month and months_paid <= extra_payment_end_month:
+        principal = principal - extra_payment
+        total_paid = total_paid + extra_payment
+
+    if principal <= 0:
+        payment = principal
+        principal = principal - payment  
+
     print(months_paid, 'Total paid', round(total_paid, 2),'Principal', round(principal,2))
-#print('Total paid', round(total_paid, 2),'Months paid', months_paid)
+    
+
+
+    
     
