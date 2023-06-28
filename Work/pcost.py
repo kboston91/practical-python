91414ce3a4ab3ed8157927cd44735ef2a87d7b74
kgb965 <kourtney.boston@austin.utexas.edu>
2023-06-28 14:36:49 -0500

    Exercises 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,18 @@
 # pcost.py
 #
+
+
+p = open('Data/portfolio.csv', 'rt')
+headers = next(p).split(',')
+print(headers)
+total_cost = 0
+
+for line in p:
+    row = line.split(',')
+    share_cost= round(int(row[1]) * float(row[2]), 2)
+    total_cost = total_cost + share_cost
+    print('The cost to buy ', row[0], ' would be ', share_cost)
+print('The cost to buy this portfolio is ', total_cost)
+
+p.close()
 # Exercise 1.27
