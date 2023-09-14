32ac381661e4664795f4a10584539d987cc68362
kgb965 <kourtney.boston@austin.utexas.edu>
2023-09-14 01:08:38 -0500

    Exercise

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,42 @@
 # report.py
-#
-# Exercise 2.4
+import csv
+
+def read_portfolio(filename):
+    portfolio = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+
+        for row in rows:
+            stock = {'name': row[0],'shares': int(row[1]),'price': float(row[2])}
+            portfolio.append(stock)
+
+    return portfolio
+
+def read_prices(filename):
+    prices = {}
+    with open(filename) as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                pass
+
+    return prices
+
+portfolio = read_portfolio('../../Work/Data/portfolio.csv')
+prices    = read_prices('../../Work/Data/prices.csv')
+
+total_cost = 0.0
+for s in portfolio:
+    total_cost += s['shares']*s['price']
+
+print('Total cost', total_cost)
+
+total_value = 0.0
+for s in portfolio:
+    total_value += s['shares']*prices[s['name']]
+
+print('Current value', total_value)
+print('Gain', total_value - total_cost)
